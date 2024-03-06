9ab69dd1c675ce179d9b6de8bc1e07065610b896
7ommyShelby <aryaalsaher@gmail.com>
2024-03-06 16:37:54 +0530

    animation

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,13 +3,23 @@ import './quotes.css'
 import data from './data.json'
 import Nav from './Nav'
 import Tab from './Tab'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import {useEffect} from "react";
+
 
 const Quotes = () => {
+
+    useEffect(() => {
+        AOS.init();
+      }, [])
 
     return (
         <>
             <Nav />
-            <section className='quotes-display'>
+            <section className='quotes-display' data-aos="fade-up"
+            >
                 {
                     data.map((e, idx) => {
                         return (
